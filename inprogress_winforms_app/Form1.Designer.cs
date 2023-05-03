3a5ca1a5b7b077103159028b58ee1d8bb8385afd
Bence Pálfi <bence.palfi@stud.uni-corvinus.hu>
2023-05-03 20:44:26 +0200

    Add project files.

---
+++ inprogress_winforms_app/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace inprogress_winforms_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_kereses = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox_mennyiseg = new System.Windows.Forms.TextBox();
+            this.button_plus = new System.Windows.Forms.Button();
+            this.button_minus = new System.Windows.Forms.Button();
+            this.button_megse = new System.Windows.Forms.Button();
+            this.button_mentes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_kereses
+            // 
+            this.textBox_kereses.Location = new System.Drawing.Point(37, 34);
+            this.textBox_kereses.Name = "textBox_kereses";
+            this.textBox_kereses.Size = new System.Drawing.Size(364, 22);
+            this.textBox_kereses.TabIndex = 0;
+            this.textBox_kereses.TextChanged += new System.EventHandler(this.textBox_kereses_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(37, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(364, 388);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox_mennyiseg
+            // 
+            this.textBox_mennyiseg.Location = new System.Drawing.Point(622, 199);
+            this.textBox_mennyiseg.Name = "textBox_mennyiseg";
+            this.textBox_mennyiseg.ReadOnly = true;
+            this.textBox_mennyiseg.Size = new System.Drawing.Size(95, 22);
+            this.textBox_mennyiseg.TabIndex = 2;
+            // 
+            // button_plus
+            // 
+            this.button_plus.Location = new System.Drawing.Point(906, 107);
+            this.button_plus.Name = "button_plus";
+            this.button_plus.Size = new System.Drawing.Size(75, 75);
+            this.button_plus.TabIndex = 3;
+            this.button_plus.Text = "+";
+            this.button_plus.UseVisualStyleBackColor = true;
+            this.button_plus.Click += new System.EventHandler(this.button_plus_Click);
+            // 
+            // button_minus
+            // 
+            this.button_minus.Location = new System.Drawing.Point(906, 212);
+            this.button_minus.Name = "button_minus";
+            this.button_minus.Size = new System.Drawing.Size(75, 75);
+            this.button_minus.TabIndex = 4;
+            this.button_minus.Text = "-";
+            this.button_minus.UseVisualStyleBackColor = true;
+            this.button_minus.Click += new System.EventHandler(this.button_minus_Click);
+            // 
+            // button_megse
+            // 
+            this.button_megse.Location = new System.Drawing.Point(802, 449);
+            this.button_megse.Name = "button_megse";
+            this.button_megse.Size = new System.Drawing.Size(109, 45);
+            this.button_megse.TabIndex = 5;
+            this.button_megse.Text = "Mégse";
+            this.button_megse.UseVisualStyleBackColor = true;
+            // 
+            // button_mentes
+            // 
+            this.button_mentes.Location = new System.Drawing.Point(954, 449);
+            this.button_mentes.Name = "button_mentes";
+            this.button_mentes.Size = new System.Drawing.Size(109, 45);
+            this.button_mentes.TabIndex = 6;
+            this.button_mentes.Text = "Mentés";
+            this.button_mentes.UseVisualStyleBackColor = true;
+            this.button_mentes.Click += new System.EventHandler(this.button_mentes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(557, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Jelenleg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(723, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "termék van készleten";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 533);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_mentes);
+            this.Controls.Add(this.button_megse);
+            this.Controls.Add(this.button_minus);
+            this.Controls.Add(this.button_plus);
+            this.Controls.Add(this.textBox_mennyiseg);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox_kereses);
+            this.Name = "Form1";
+            this.Text = "In progress készlet módosítás";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_kereses;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox_mennyiseg;
+        private System.Windows.Forms.Button button_plus;
+        private System.Windows.Forms.Button button_minus;
+        private System.Windows.Forms.Button button_megse;
+        private System.Windows.Forms.Button button_mentes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ inprogress_winforms_app/Termek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inprogress_winforms_app
+{
+    internal class Termek
+    {
+        public int id;
+        public string nev;
+        public int keszlet;
+        public string inventory_id;
+
+    }
+}
